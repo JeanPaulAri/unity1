624143591c43da9c8b7fa5f74537f97760ce000e
Jean Paul Ari Maldonado <94414304+JeanPaulAri@users.noreply.github.com>
2022-11-11 10:35:09 -0500

    first commit

---
+++ Assets/Scripts/JhonSystem.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JhonSystem : MonoBehaviour
+{
+    private float _horizontal;
+    private Rigidbody2D _rigidbody2D;
+    private bool _onGround;
+    public float jumpForce;
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        _horizontal = Input.GetAxisRaw("Horizontal");
+         
+        Debug.DrawRay(transform.position,Vector3.down * 0.1f, Color.red);
+        _onGround = Physics2D.Raycast(transform.position, Vector3.down, 0.1f);
+
+        if (Input.GetKeyDown(KeyCode.Space) && _onGround)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        _rigidbody2D.AddForce(Vector2.up * jumpForce);
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.velocity = new Vector2(_horizontal, _rigidbody2D.velocity.y);
+    }
+}
